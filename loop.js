4b2580992598d6a32df14b1af648145829892bcd
sohagroy7223 <freelancersohagroy@gmail.com>
2025-11-03 16:48:07 +0600

    adad for loop in object

---
+++ loop.js
@@ -0,0 +1,24 @@
+const mobile = {
+    brand: "apple,",
+    price: "26000,",
+    battery: "5000ms,",
+    camera: "108mp.",
+};
+for (let phone in mobile) {
+    console.log(mobile[phone]);
+}
+
+// const phone = Object.values(mobile);
+// console.log(phone);
+
+// const phone = Object.keys(mobile);
+
+// console.log(phone);
+// for (array of phone) {
+//     console.log(array);
+// }
+
+// for (const phone in mobile) {
+//     console.log(phone);
+//     console.log(mobile[phone]);
+// }
